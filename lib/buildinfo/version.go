2c89b8edb72c6d5b02137e2e342a4cb69c72c22f
shepard <eviltomorrow@163.com>
2022-11-09 15:08:01 +0800

    init

--- lib/buildinfo/version.go
+++ lib/buildinfo/version.go
@@ -6,21 +6,22 @@ import (
 )
 
 var (
-	MainVersion string
-	GoVersion   = runtime.Version()
-	GoOSArch    = runtime.GOOS + "/" + runtime.GOARCH
-	GitSha      string
-	GitTag      string
-	GitBranch   string
-	BuildTime   string
+	AppName     string = "Unknown"
+	MainVersion string = "Unknown"
+	GoVersion          = runtime.Version()
+	GoOSArch           = runtime.GOOS + "/" + runtime.GOARCH
+	GitSha      string = "Unknown"
+	GitTag      string = "Unknown"
+	GitBranch   string = "Unknown"
+	BuildTime   string = "Unknown"
 )
 
 func GetVersion() string {
-	return fmt.Sprintf(`Current Version: %s
+	return fmt.Sprintf(`Main Version(%s): %s
 Git Sha: %s
 Git Tag: %s
 Git Branch: %s
 Go Version: %s
 GO OS/Arch: %s
-Build Time: %s`, MainVersion, GitSha, GitTag, GitBranch, GoVersion, GoOSArch, BuildTime)
+Build Time: %s`, AppName, MainVersion, GitSha, GitTag, GitBranch, GoVersion, GoOSArch, BuildTime)
 }
